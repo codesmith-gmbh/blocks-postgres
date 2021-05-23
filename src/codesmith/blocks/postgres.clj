cd98afb4f732956939bffa566003e84183ac89e5
Stanislas Nanchen <240862+stanislas@users.noreply.github.com>
2021-05-23 17:34:51 +0200

    A Postgres Block.

---
+++ src/codesmith/blocks/postgres.clj
@@ -0,0 +1,35 @@
+(ns codesmith.blocks.postgres
+  (:require [codesmith.blocks :as cb]
+            [integrant.core :as ig]
+            [next.jdbc.connection :as conn])
+  (:import [javax.sql DataSource]
+           [org.flywaydb.core Flyway]
+           [com.zaxxer.hikari HikariDataSource]))
+
+(defn migrate-db! [^DataSource ds]
+  (let [^Flyway flyway (-> (Flyway/configure) (.dataSource ds) (.load))]
+    (.migrate flyway)))
+
+(defn config->db-spec [{:keys [connection-url]}]
+  {:jdbcUrl connection-url})
+
+(defmethod cb/typed-block-transform
+  [::cb/postgres :external]
+  [block-key {:keys [application environment]} ig-config]
+  (if (not (::external ig-config))
+    (let [connection-url-key [:elements.block/secret :elements.block.postgres/connection-url]]
+      (assoc ig-config
+        ::external {:connection-url (ig/ref connection-url-key)}
+        connection-url-key {:application    application
+                            :environment    environment
+                            :block-name     (keyword (name block-key))
+                            :parameter-name :connection-url}))
+    ig-config))
+
+(defmethod ig/init-key ::external
+  [_ config]
+  (let [^HikariDataSource ds (conn/->pool HikariDataSource (config->db-spec config))]
+    (migrate-db! ds)
+    ds))
+
+(derive ::external ::cb/postgres)
